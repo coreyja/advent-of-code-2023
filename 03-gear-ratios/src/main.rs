b2859628ea3459c6075a661f40f7227a2ad72a52
Corey Alexander <coreyja@gmail.com>
2023-12-03 01:06:09 -0500

    Day 3 Gear Ratios

--- 03-gear-ratios/src/main.rs
+++ 03-gear-ratios/src/main.rs
@@ -33,9 +33,31 @@ impl GridNumber {
 
         false
     }
+
+    fn is_next_to(&self, (i, j): (usize, usize)) -> bool {
+        if self.y.abs_diff(i) > 1 {
+            return false;
+        }
+
+        if j < self.x_start.saturating_sub(1) || j > self.x_end + 1 {
+            return false;
+        }
+
+        true
+    }
 }
 
 fn part_1(input: &str) -> Result<u32> {
+    let (array, grid_numbers) = parse_grid_numbers(input);
+
+    Ok(grid_numbers
+        .iter()
+        .filter(|gn| gn.is_part_number(&array))
+        .map(|gn| gn.num)
+        .sum::<u32>())
+}
+
+fn parse_grid_numbers(input: &str) -> (Vec<Vec<char>>, Vec<GridNumber>) {
     let array = input
         .lines()
         .map(|l| l.chars().collect::<Vec<_>>())
@@ -93,12 +115,34 @@ fn part_1(input: &str) -> Result<u32> {
 
         i += 1;
     }
+    (array, grid_numbers)
+}
 
-    Ok(grid_numbers
-        .iter()
-        .filter(|gn| gn.is_part_number(&array))
-        .map(|gn| gn.num)
-        .sum::<u32>())
+fn part_2(input: &str) -> u32 {
+    let (array, grid_numbers) = parse_grid_numbers(input);
+
+    let mut gear_ratios = vec![];
+    let mut i = 0;
+    while i < array.len() {
+        let mut j = 0;
+        while j < array[i].len() {
+            let c = array[i][j];
+            if c == '*' {
+                let neighbors = grid_numbers
+                    .iter()
+                    .filter(|gn| gn.is_next_to((i, j)))
+                    .collect::<Vec<_>>();
+
+                if neighbors.len() == 2 {
+                    gear_ratios.push(neighbors[0].num * neighbors[1].num)
+                }
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+
+    gear_ratios.iter().sum()
 }
 
 fn main() -> Result<()> {
@@ -111,6 +155,12 @@ fn main() -> Result<()> {
     let my_part_1_ans = part_1(my_input)?;
 
     dbg!(my_part_1_ans);
+
+    let sample_part_2_ans = part_2(sample_input);
+    dbg!(sample_part_2_ans);
+
+    let my_part_2_ans = part_2(my_input);
+    dbg!(my_part_2_ans);
 
     Ok(())
 }
