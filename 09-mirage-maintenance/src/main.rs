eed913ee41cd781790bcd907d409af508d2a5fbd
Corey Alexander <coreyja@gmail.com>
2023-12-09 22:06:43 -0500

    Day 09 Part 2

--- 09-mirage-maintenance/src/main.rs
+++ 09-mirage-maintenance/src/main.rs
@@ -14,18 +14,35 @@ fn part_1(input: &str) -> i64 {
     histories.into_iter().map(|h| h.next_item()).sum()
 }
 
+fn part_2(input: &str) -> i64 {
+    let histories = input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(History)
+        .collect::<Vec<_>>();
+
+    histories.into_iter().map(|h| h.previous_item()).sum()
+}
+
 impl History {
     fn next_item(&self) -> i64 {
         next_number(&self.0)
     }
+
+    fn previous_item(&self) -> i64 {
+        let mut reversed = self.0.clone();
+        reversed.reverse();
+
+        next_number(&reversed)
+    }
 }
 
 fn next_number(arr: &[i64]) -> i64 {
-    let differences = arr
-        .windows(2)
-        .map(|w| w[1] - w[0])
-        // .map(|d| d as i64)
-        .collect::<Vec<_>>();
+    let differences = arr.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
 
     let last = arr.last().unwrap();
 
@@ -45,4 +62,10 @@ fn main() {
     let my_input = include_str!("my.input");
     let my_part_1_ans = part_1(my_input);
     dbg!(my_part_1_ans);
+
+    let sample_part_2_ans = part_2(sample_input);
+    dbg!(sample_part_2_ans);
+
+    let my_part_2_ans = part_2(my_input);
+    dbg!(my_part_2_ans);
 }
