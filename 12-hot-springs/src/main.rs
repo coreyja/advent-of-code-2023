d8f0fecbbb96f10d692340208cdc334a4ef5bd53
Corey Alexander <coreyja@gmail.com>
2023-12-18 20:40:29 -0500

    Part 2 brute force

--- 12-hot-springs/src/main.rs
+++ 12-hot-springs/src/main.rs
@@ -20,6 +20,26 @@ impl Row {
     fn count_possible(&self) -> usize {
         count_possible(&self.cells, &self.broken_sets)
     }
+
+    fn expand(&self) -> Self {
+        let mut new_cells = vec![];
+        for i in 0..5 {
+            new_cells.append(&mut self.cells.clone());
+            if i != 4 {
+                new_cells.push(Cell::Unknown);
+            }
+        }
+
+        let new_broken_sets = vec![self.broken_sets.clone(); 5]
+            .into_iter()
+            .flatten()
+            .collect();
+
+        Self {
+            cells: new_cells,
+            broken_sets: new_broken_sets,
+        }
+    }
 }
 
 fn to_sets(cells: &[Cell]) -> Vec<usize> {
@@ -37,7 +57,7 @@ fn to_sets(cells: &[Cell]) -> Vec<usize> {
             Cell::Broken => {
                 current_set += 1;
             }
-            Cell::Unknown => panic!("Unknown cell when counting sets"),
+            Cell::Unknown => return sets,
         }
     }
 
@@ -48,12 +68,41 @@ fn to_sets(cells: &[Cell]) -> Vec<usize> {
     sets
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum PrefixMatch {
+    Prefix,
+    Full,
+}
+
+fn prefix_match(cell_sets: &[usize], broken_sets: &&[usize]) -> Option<PrefixMatch> {
+    if cell_sets.len() > broken_sets.len() {
+        return None;
+    }
+
+    for (i, c) in cell_sets.iter().enumerate() {
+        if c != &broken_sets[i] {
+            return None;
+        }
+    }
+
+    if cell_sets.len() == broken_sets.len() {
+        Some(PrefixMatch::Full)
+    } else {
+        Some(PrefixMatch::Prefix)
+    }
+}
+
 fn count_possible(cells: &[Cell], broken_sets: &[usize]) -> usize {
+    let cell_sets = to_sets(cells);
+
+    let prefix_match = prefix_match(&cell_sets, &broken_sets);
+    let Some(prefix_match) = prefix_match else {
+        return 0;
+    };
+
     let unknown_index = cells.iter().position(|c| *c == Cell::Unknown);
     let Some(unknown_index) = unknown_index else {
-        let cell_sets = to_sets(cells);
-
-        if cell_sets == broken_sets {
+        if prefix_match == PrefixMatch::Full {
             return 1;
         } else {
             return 0;
@@ -96,6 +145,13 @@ fn part_1(input: &str) -> usize {
     rows.into_iter().map(|r| r.count_possible()).sum()
 }
 
+fn part_2(input: &str) -> usize {
+    let rows = input.lines().map(Row::parse).collect::<Vec<_>>();
+    let expanded_rows = rows.iter().map(|r| r.expand()).collect::<Vec<_>>();
+
+    expanded_rows.into_iter().map(|r| r.count_possible()).sum()
+}
+
 fn main() {
     let sample_input = include_str!("sample.input");
     let sample_part_1_ans = part_1(sample_input);
@@ -104,4 +160,10 @@ fn main() {
     let my_input = include_str!("my.input");
     let my_part_1_ans = part_1(my_input);
     dbg!(my_part_1_ans);
+
+    let sample_part_2_ans = part_2(sample_input);
+    dbg!(sample_part_2_ans);
+
+    let my_part_2_ans = part_2(my_input);
+    dbg!(my_part_2_ans);
 }
